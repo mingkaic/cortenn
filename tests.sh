8d31c8f4300e7ab8f25d3b4e65dbee441e7f6f0c
RaggleDodo <rododorodo@gmail.com>
2019-01-02 21:43:32 -0800

    remove asan from ptest, replace with valgrind instead

--- tests.sh
+++ tests.sh
@@ -9,7 +9,11 @@ set -e
 
 # ===== Run Gtest =====
 echo "===== TESTS =====";
-bazel test --config asan --config gtest //...
+bazel test --config asan --config gtest //bwd:test
+bazel test --config asan --config gtest //opt:test
+bazel test --config asan --config gtest //llo:ctest
+bazel test --run_under='valgrind --leak-check=full' //llo:ptest
+bazel test --config asan --config gtest //pbm:test
 
 # ===== Check Docs Directory =====
 echo "===== CHECK DOCUMENT EXISTENCE =====";
